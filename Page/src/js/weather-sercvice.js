39b1ba23c69bab7c79ab1a9ba83a91cdf504554c
Basil Koval <lodupu8989@g,gmail.com>
2020-06-08 17:37:03 +0300

    ready

--- Page/src/js/weather-sercvice.js
+++ Page/src/js/weather-sercvice.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  'http://api.openweathermap.org/data/2.5/weather?appid=454c40cf5a0b5b005766fe8d9e827229';
+  'https://api.openweathermap.org/data/2.5/weather?appid=454c40cf5a0b5b005766fe8d9e827229';
 
 export default {
   timeZone: 0,
